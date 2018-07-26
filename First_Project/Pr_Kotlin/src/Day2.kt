a1bd8ddd4b1e5e52e0077d684145ba2984cd59c7
onemask <onemask@naver.com>
2018-07-26 12:38:10 +0900

    Chapt30_Function

--- First_Project/Pr_Kotlin/src/Day2.kt
+++ First_Project/Pr_Kotlin/src/Day2.kt
@@ -1,28 +1,30 @@
 /***
- * 29 _ ****Lable 사용. 
+ * 30 _ *****Function.
  *
  ***/
+//(kotlin에서 Unit은 무엇을 말하는겨?
 
 
 fun main(args:Array<String>):Unit {
 
-    var x=0
-    var y=0
-
-    outer@ while(x<=20){
-        y=0
-        while(y<=20){
-            if(x+y == 15 && x-y ==5)
-                break@outer
-            y+=1
-        }
-        x+=1
-    }
-    println("x : $x, y : $y")
-
-    //(kotlin에서 Unit은 무엇을 말하는겨?
+    println(myFunction())
+    println(myFunction() + 10)
+    println(myFunction2())
 
 }
 
+fun myFunction():Int{
+    val a=3
+    val b=6
+    println("a : $a b: $b")
+    return a+b //함수를 호출했던 위치로 돌아감.
 
+}
 
+fun myFunction2()=3.0+7 //함수의 반환 타입, Return값도 생략 가능(추론가능시)
+
+//함수 선언하기.
+// fun 식별자() : 반환타입
+//{
+//      수행할 문장 .
+//}
