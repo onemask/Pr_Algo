08806e82f8e3ed310b8b1fea327882973f55d87a
onemask <onemask@naver.com>
2018-07-26 12:10:37 +0900

    Continue

--- First_Project/Pr_Kotlin/src/Day2.kt
+++ First_Project/Pr_Kotlin/src/Day2.kt
@@ -8,10 +8,15 @@ fun main(args:Array<String>):Unit {
 
   var i =1;
 
-    while(i<10){
-        println("i는 $i")
-        i+=i
+    while (i<10){
+        i+=1
+        if(i%2==0)
+            continue //continue 아래의 문장을 모두 skip 한다.
+            print(i)
+
+
     }
+
 
 
     //(kotlin에서 Unit은 무엇을 말하는겨?
