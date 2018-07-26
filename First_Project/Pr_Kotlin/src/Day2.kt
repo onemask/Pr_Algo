3e55a09e2e7a0ef343789737b1f8d0eaad599037
onemask <onemask@naver.com>
2018-07-26 11:56:45 +0900

    val grade :Char = when(score/10)

--- First_Project/Pr_Kotlin/src/Day2.kt
+++ First_Project/Pr_Kotlin/src/Day2.kt
@@ -10,17 +10,17 @@ fun main(args:Array<String>):Unit {
 
    val score = 99
 
-    when (score/ 10){           //비교의 대상이 되는 타깃을 정한다.
+   val grade: Char = when (score/ 10){           //비교의 대상이 되는 타깃을 정한다.
 
-        6->{println("D")}   //score/10 ==6 인지.
-        7->{println("C")}
-        8->{println("B")}
-        9,10->{println("A")} //쉼표를 이용하여 값 여러개를 지정할 수 있다.
-        else ->{println("F")} // 아무것도 아닐때.
+        6->'D'   //score/10 ==6 인지.
+        7->'C'
+        8->'B'
+        9,10->'A'//쉼표를 이용하여 값 여러개를 지정할 수 있다.
+        else ->'F' // 아무것도 아닐때.
     }
-    println("Test")
+    println("The grade is $grade")
 
-
+    //(kotlin에서 Unit은 무엇을 말하는겨?
 
 }
 
