ff51e4120142b3092496ea2f2502989b32c8f990
onemask <onemask@naver.com>
2018-07-26 12:04:22 +0900

    Using WHile

--- First_Project/Pr_Kotlin/src/Day2.kt
+++ First_Project/Pr_Kotlin/src/Day2.kt
@@ -1,24 +1,18 @@
 /***
- *  23 _ when if
- *  when (비교할 대상 선언)
- *  찾으면 밖으로 나온다.
+ * 25 _ while
  *
- * */
+ ***/
 
 
 fun main(args:Array<String>):Unit {
 
-   val score = 99
+  var i =1;
 
-   val grade: Char = when (score/ 10){           //비교의 대상이 되는 타깃을 정한다.
-
-        6->'D'   //score/10 ==6 인지.
-        7->'C'
-        8->'B'
-        9,10->'A'//쉼표를 이용하여 값 여러개를 지정할 수 있다.
-        else ->'F' // 아무것도 아닐때.
+    while(i<10){
+        println("i는 $i")
+        i+=i
     }
-    println("The grade is $grade")
+
 
     //(kotlin에서 Unit은 무엇을 말하는겨?
 
