0dd0eed7c2fe73688443660250a3297e84b18e29
onemask <onemask@naver.com>
2018-07-26 12:49:32 +0900

    Chapt 31. Function

--- First_Project/Pr_Kotlin/src/Day2.kt
+++ First_Project/Pr_Kotlin/src/Day2.kt
@@ -1,30 +1,21 @@
 /***
- * 30 _ *****Function.
+ * 31 _ *****Function.
  *
  ***/
 //(kotlin에서 Unit은 무엇을 말하는겨?
 
 
 fun main(args:Array<String>):Unit {
-
-    println(myFunction())
-    println(myFunction() + 10)
-    println(myFunction2())
-
+    println(cToF(30))
+    println(getAverage(89,96))
 }
 
-fun myFunction():Int{
-    val a=3
-    val b=6
-    println("a : $a b: $b")
-    return a+b //함수를 호출했던 위치로 돌아감.
-
+fun  getAverage(a: Int,b:Int): Double {
+    return (a+b)/2.0
 }
 
-fun myFunction2()=3.0+7 //함수의 반환 타입, Return값도 생략 가능(추론가능시)
-
-//함수 선언하기.
-// fun 식별자() : 반환타입
-//{
-//      수행할 문장 .
-//}
+//매개변수는 무조건 val로 선언된다. 
+fun cToF(i: Int): Double {
+    return i*1.8 +32
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
