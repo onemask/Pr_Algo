9105bac787b9d25ce37cd997fcb1a00f03ddd491
onemask <onemask@naver.com>
2018-07-26 13:28:47 +0900

    Default Prameter

--- First_Project/Pr_Kotlin/src/Day2.kt
+++ First_Project/Pr_Kotlin/src/Day2.kt
@@ -1,21 +1,32 @@
 /***
- * 31 _ *****Function.
+ * 33 _ 디폴트인수
  *
  ***/
-//(kotlin에서 Unit은 무엇을 말하는겨?
 
 
-fun main(args:Array<String>):Unit {
-    println(cToF(30))
-    println(getAverage(89,96))
+//Unit 반환값이 필요없을 때 사용.
+fun main(args:Array<String>) {
+
+   println(getAverage(89,96)) //print  92.5
+   getAverage(100,50,true) //75
+   println(getAverage(90))        //print 45
+    getAverage(66,print=true)       //33
+    getAverage(print=true)          //0
+    getAverage(print=true,a=10,b=30)    //20
+
 }
 
-fun  getAverage(a: Int,b:Int): Double {
-    return (a+b)/2.0
+//매개 변수를 선언과 동시에 초기화,
+fun  getAverage(a:Int=0,b:Int=0,print:Boolean=false):Double{
+ val result = (a+b)/2.0
+    if(print)
+        println(result)
+    return result
 }
 
-//매개변수는 무조건 val로 선언된다. 
-fun cToF(i: Int): Double {
-    return i*1.8 +32
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-}
+//매개변수의 이름을 지정한 인수는 일반 인수들보다  항상 오른쪽에 있어야한다.
+//getAverage(print=true,10,30)
+//getAverage(10,print=true,30)
+// !Error! : Mixing named and positioned arguments is not allowed (이름이 지정된 인수와 위치로 지정된 인수는 혼재될수 없다.)
+
+
