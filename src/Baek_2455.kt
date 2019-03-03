357deb14e12aae4c966721affcfe2ae7874fc0fb
onemask <onemask@naver.com>
2019-03-03 17:53:36 +0900

    init

---
+++ src/Baek_2455.kt
@@ -0,0 +1,24 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun main(args : Array<String>){
+
+    var check  = 0
+    val read = Scanner(System.`in`)
+    var people : ArrayList<Int> = arrayListOf(0)
+
+    do{
+        val minus = read.nextInt() * (-1)
+        val plus = read.nextInt()
+        people.add((people.last())+minus+plus)
+        if(minus * plus == 0){
+            check++
+        }
+    }while(check<2)
+
+
+    println(people.max())
+
+
+
+}
