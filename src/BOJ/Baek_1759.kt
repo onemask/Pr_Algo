55ba19e72142adfc29709db343b7e575b54a5fa3
onemask <onemask@naver.com>
2019-03-19 13:36:38 +0900

    백준 1759 input받는거 readLined으로 변경.

---
+++ src/BOJ/Baek_1759.kt
@@ -0,0 +1,30 @@
+package BOJ
+
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.Reader
+import java.lang.StringBuilder
+import java.nio.Buffer
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+fun main(args: Array<String>) {
+
+    val arrays = readLine()
+    val inpuuts = readLine()
+
+
+    println("$arrays $inpuuts")
+
+
+    var inputs: ArrayList<Char>? = arrayListOf()
+//
+//    for (idx in 0..c - 1) {
+//        // inputs?.add(str[idx])
+//    }
+
+    println(inputs)
+
+}
